b30654307551a2517f70cad1e8eb36164b3035e7
Nguyen Huynh Nhat Minh <84497427+nhnMinhFPT@users.noreply.github.com>
2022-02-18 22:56:25 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -38,8 +38,32 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.album),
+                  title: Text('The Enchanted Nightingale'),
+                  subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('BUY TICKETS'),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                    TextButton(
+                      child: const Text('LISTEN'),
+                      onPressed: () {/* ... */},
+                    ),
+                    const SizedBox(width: 8),
+                  ],
+                ),
+              ]
+          )
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
