492795ed381198a67baf1071857f74e8919f343e
nhnMinhFPT <84497427+nhnMinhFPT@users.noreply.github.com>
2022-02-23 15:52:15 +0700

    update home

--- lib/screens/home_screen/home.dart
+++ lib/screens/home_screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gsms_mobileapp_swd/widgets/menu_card.dart';
+import 'package:flutter/rendering.dart';
+import 'package:gsms_mobileapp_swd/widgets/home_item_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -40,51 +41,111 @@ class _HomeScreenState extends State<HomeScreen> {
             mainAxisSpacing: 5,
             crossAxisCount: 3,
             children: const <Widget>[
-              MenuCard(
+              HomeItemCard(
+                cardIcon: Icon(Icons.shopping_cart_outlined),
                 cardName: 'Order',
+                amount: 0,
                 routeName: '/order_list',
               ),
-              MenuCard(
+              HomeItemCard(
+                cardIcon: Icon(Icons.grid_view),
                 cardName: 'Product',
+                amount: 0,
                 routeName: '/',
               ),
-              MenuCard(
-                cardName: 'Brand',
-                routeName: '/',
-              ),
-              MenuCard(
-                cardName: 'Employee',
-                routeName: '/',
-              ),
-              MenuCard(
+              HomeItemCard(
+                cardIcon: Icon(Icons.emoji_emotions_outlined),
                 cardName: 'Customer',
+                amount: 0,
                 routeName: '/',
               ),
-              MenuCard(
-                cardName: 'Shop',
+              HomeItemCard(
+                cardIcon: Icon(Icons.people_alt_outlined),
+                cardName: 'Employee',
+                amount: 0,
+                routeName: '/',
+              ),
+              HomeItemCard(
+                cardIcon: Icon(Icons.remove_shopping_cart_outlined),
+                cardName: 'Sold out',
+                amount: 0,
+                routeName: '/',
+              ),
+              HomeItemCard(
+                cardIcon: Icon(Icons.trending_down_outlined),
+                cardName: 'Low in Stock',
+                amount: 0,
                 routeName: '/',
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+            child: Container(
+              constraints: const BoxConstraints(
+                maxHeight: 300,
+                maxWidth: 100,
+              ),
+              child: Card(
+                elevation: 5,
+                shadowColor: Colors.blueGrey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0, left: 20.0),
+                      child: Text(
+                        "Product Sales",
+                        style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.green,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: ElevatedButton(
+                              onPressed: (){},
+                              child: Text("Day")
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: ElevatedButton(
+                              onPressed: (){},
+                              child: Text("Month")
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8.0),
+                          child: ElevatedButton(
+                              onPressed: (){},
+                              child: Text("Year")
+                          ),
+                        ),
+                      ],
+                    ),
+                    SalesChart()
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.holiday_village_outlined),
-            label: 'Shop',
+            icon: Icon(Icons.notifications_none),
+            label: 'Notification',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart_outlined),
-            label: 'Order',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
             label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_outline),
-            label: 'Account',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person_outline),
@@ -96,5 +157,21 @@ class _HomeScreenState extends State<HomeScreen> {
         onTap: _onItemTapped,
       ),
     );
+  }
+}
+
+class SalesChart extends StatefulWidget {
+  const SalesChart({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SalesChart> createState() => _SalesChartState();
+}
+
+class _SalesChartState extends State<SalesChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Chart Zone");
   }
 }
